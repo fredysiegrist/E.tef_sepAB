090299d15da31b76b2b7324a6277a493f5aceabb
fredysiegrist <fredy.siegrist@students.unibe.ch>
2015-09-07 16:13:37 +0200

    Read DAG file and give out first two lines

--- import_DAGChainer.py
+++ import_DAGChainer.py
@@ -1,3 +1,7 @@
 __author__ = 'fsiegris'
 #date: 07.09.2015
 
+file = open('../../i1sz/22790_24796.CDS-CDS.last.tdd10.cs0.filtered.dag.all.go', 'r+')
+print(file.readline())
+print(file.readline())
+
