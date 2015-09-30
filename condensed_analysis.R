9737acc22592d625006e33bf51ee8730874cea2d
fredysiegrist <fredy.siegrist@students.unibe.ch>
2015-09-30 17:12:00 +0200

    version 0.18.0 R plot & master file approach _______________________________________________________ Coordinate system needs final testing! Version checks for block number at chromosome check in first if statement

--- condensed_analysis.R
+++ condensed_analysis.R
@@ -17,12 +17,13 @@ statdir <- file.path("output")
 
 
 # Adressing the files
-cond <- read.delim(file="stretches_condensed2.csv", header=FALSE, skip=0, comment.char = "")
+cond <- read.delim(file="stretches_condensed3.csv", header=FALSE, skip=0, comment.char = "")
 colnames(cond) <- c("chr", "start", "end", "ori", "scaffold", "sstart", "send", "sori", "stretch", "number", "score", "reversion", "gir", "block", "position")
 chrlen <- c(73840612, 77932577, 74440842, 68034345, 62352331, 62208772, 64342021, 55460251, 59635592, 60981625)
 
 require('colorspace')
 seqcol <- heat_hcl(16, c=c(80,30), l = c(30,90), power = c(1/5, 1.5))
+scorecol <- terrain_hcl(20, c=c(65,0), l=c(45, 95), power=c(1/3, 1.5))
 seqcol[1] <- "#3C3CEC"
 
 pdf(file=paste(getwd(),"/output/density_map_chromosomes_cond4.pdf", sep=""), paper="a4r", width = (2967/150)/2.54, height = (2099/150)/2.54)
@@ -41,5 +42,29 @@ for (chrno in 1:10) {
 dev.off()
 
 # Find the stretches that cover more than 1 billion nucleotides:
+cond[(cond[,3]-cond[,2])>2000000,]
 
-cond[(cond[,3]-cond[,2])>10000000,]
+# Plot the s the match length on Sorghum chromosome against scaffold length of E.tef for found stretches
+plot(abs(cond[,3]-cond[,2]), abs(cond[,6]-cond[,7]), pch="•", cex=1, col=seqcol[cond[,15]], xlab="Sorghum chromosome legnth", ylab="E. tef scaffold length", main="scaffold length vs. match length", log="xy", xlim=c(5e+3, 1e+7), ylim=c(1e+3, 1e+6))
+abline(0, glm(log(abs(cond[,6]-cond[,7])) ~ log(abs(cond[,3]-cond[,2]))-1)$coefficients,  lwd=2, col="yellow4")
+abline(0, 1,  lwd=2, col="yellow2", lty=3)
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted", lwd = par("lwd"), equilogs = TRUE)
+length(levels(factor(.bincode(cond[,11], seq(75, 3500, by=100)))))
+points(abs(cond[,3]-cond[,2]), abs(cond[,6]-cond[,7]), pch="•", cex=0.6, col=scorecol[as.numeric(levels(factor(.bincode(cond[,11], seq(75, 3500, by=100)))))])
+legend(2e+6, 5e+3, legend=c("први   stretch","други stretch","шести stretch","score   75 -   175","score 476 -   575","score 976 - 1075"), col=c(seqcol[c(1,2,6)], scorecol[c(1, 5, 10)]), pch=c(rep(c(16, 20), each=3)))
+(75+100*(as.numeric(levels(factor(.bincode(cond[,11], seq(75, 3500, by=100)))))-1))[c(1, 5, 10)]
+
+
+# reconstruct the master file
+bestlist <- cond[cond[,15]==1,]
+bestlist <- bestlist[order((bestlist[,1]*1e10)+bestlist[,2]+bestlist[,3]),]
+
+    # scaffold7112 appears on chromosome 1 while it is attributed to chromosome 2 in the master list.
+cond[cond$scaffold=="scaffold7112",]
+
+# RECONSTRUCTION
+
+bestlist[,c(1, 5, 12)]
+
+# read in the master file for comparision and refiltering
+master <- read.delim(file="master_22790_24796.CDS-CDS.last.tdd10.cs0.filtered.dag.all.go_D20_g10_A3.aligncoords.gcoords_ct0.w1000.spa1.sr.cs1.csoS.log.nsd.spa_info.txt", header=TRUE)
